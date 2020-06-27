f5cc70c3201c83dac2270e8a774ed63742fc3641
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2020-06-27 12:57:50 -0700

    changes from code review part 1

--- main.py
+++ main.py
@@ -5,64 +5,74 @@ import office2john
 import bitstring
 import numpy as np
 
-import arrays
+from arrays import * as hash_tables
 
+password = "testpassword"
 
-password = b'testpassword'
 
 def password_verifier(password):
-    verifier = 0x0000
-    
+    verifier = b"0x0000"
+
     password_array = bitstring.BitArray([])
-    password_array.append(len(password))         
+    password_array[0] = len(password_array)
+    password_array.append(password)
 
-    for byte in password_array[::-1]:
-
+    for password_byte in reversed(password_array):
         intermediate1 = 0
 
-        if verifier & 0x4000 == 0x0000:
-            intermediate1 = 0
-        else:
+        if verifier & b"0x4000" != b"0x0000":
             intermediate1 = 1
 
-            
-        intermediate2a = bitstring.BitArray(verifier * 2)
-        intermediate2 = (verifier * 2) & 0x7fff
+        intermediate2 = verifier * 2
+        intermediate2 = (verifier * 2) & 0x7FFF
         intermediate2 = 0
         intermediate3 = intermediate1 ^ intermediate2
         verifier = intermediate3 ^ 0xCE4B
         break
     return verifier ^ 0xCE4B
 
-def create_xor_array(password):
-    xor_key = xor_array #this one doesnt make too much sense #SET XorKey TO CreateXorKey_Method1(Password
-    index = password.length #questionable line
 
-
-    if index % 1:
-        temp = 0x7fff
+def create_xor_array_method1(password):
+    xor_key = create_xor_key_method1(
+        password
+    )  # this one doesnt make too much sense #SET XorKey TO CreateXorKey_Method1(Password
+    index = password.length  # questionable line
+    obfuscation_array = list(hash_tables.obfuscation_array)
 
     if index % 2 == 1:
-        temp = 0x7fff
-        obfuscation_array = PadArray #I do not know how to set this one #SET ObfuscationArray[Index] TO XorRor(PadArray[0], Temp)
+        temp = 0x7FFF # set temp to msb of xor_key
+        obfuscation_array[index] = xor_ror(list(PadArray)[0], temp)  # I do not know how to set this one #SET ObfuscationArray[Index] TO XorRor(PadArray[0], Temp)
+        index -= 1
+        # temp = lsb of xor_key
+        password_last_char = password[password.length - 1]
+        obfuscation_array[index] = xor_ror(password_last_char, temp)
 
+    while index > 0:
+        index -= 1
+        
+
+    if index % 1:
+        temp = 0x7FFF
     index = index - 1
 
-def xor_key(password):
-    xor_keys = InitialCode(len(password) - 1)
-    
+
+def create_xor_key_method1(password):
+    xor_keys = hash_tables.InitialCode(len(password) - 1)
+
     current_element = 0x00000068
 
     for char in password[::-1]:
         if char and 0x40 != 0:
-            xor_keys =  xor_key | XorMatrix
+            xor_keys = xor_key | XorMatrix
         else:
             char = char * 2
             current_element - 1
     return xor_keys
 
+
 def xor_ror(byte1, byte2):
     return ror(byte1 ^ byte2)
+
 
 def ror(byte):
     temp1 = byte / 2
@@ -70,22 +80,25 @@ def ror(byte):
     temp3 - temp1 | temp2
     return temp3 % 0x100
 
-#def encrypt_data(password, data, XorArrayIndex):
-    
-    #for index in data.length:
-        
-    
-            
-    
 
-    
+# def encrypt_data(password, data, XorArrayIndex):
+
+# for index in data.length:
+
+
+def setBitNumber(n):
+    if n == 0:
+        return 0
+
+    msb = 0
+    while n > 0:
+        n = int(n / 2)
+        msb += 1
+
+    return 1 << msb
+
 
 if __name__ == "__main__":
-    #password = os.system("python3 office2john.py easypasswd.xlsx")
-    #print(password_verifier)
-    password_verifier("testpassword")
+    password = os.system("python3 office2john.py easypasswd.xlsx")
     print(password_verifier)
-    print(create_xor_array)
-    print(xor_key)
-    print(xor_ror)
-    print(ror)
+    password_verifier("testpassword")
